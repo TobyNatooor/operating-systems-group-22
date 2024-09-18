37cc8c2263e509c58e7e3dafb3cc99b8a514b925
max <maxwork.bm@gmail.com>
2024-09-18 10:22:13 +0200

    test

--- assignment_1/test.sh
+++ assignment_1/test.sh
@@ -5,6 +5,20 @@
 in="abbabaq"
 out="0,3,5;"
 
-[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED_1" || echo "FAILED_1"
 
+in="acccaa"
+out="4,5"
+
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED_2" || echo "FAILED_2"
+
+in="bbbbaa"
+out="4,5"
+
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED_3" || echo "FAILED_3"
+
+in="aaaaacca"
+out="0,01,2,7"
+
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED_4" || echo "FAILED_4"
 
