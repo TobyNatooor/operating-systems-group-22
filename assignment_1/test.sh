0431466dc8a44aabce526cb935760c5b06196c74
max <maxwork.bm@gmail.com>
2024-09-18 11:16:39 +0200

    syntax bugfix2

--- assignment_1/test.sh
+++ assignment_1/test.sh
@@ -18,7 +18,7 @@ out="4,5"
 [[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED_3" || echo "FAILED_3"
 
 in="aaaaacca"
-out="0,0,1,2,7"
+out="0,1,2,7"
 
 [[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED_4" || echo "FAILED_4"
 
